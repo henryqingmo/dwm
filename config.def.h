d57f548f4fe76d7f7ba2af56547feb9b1fdba469
henryqingmo <henryqingmo@gmail.com>
2022-12-10 15:57:13 +1100

    configured scratchpad

--- config.def.h
+++ config.def.h
@@ -59,8 +59,12 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "Alacritty", NULL,   NULL,           0,         0,          1,		   0,		 -1 },
+	{ "Alacritty",NULL,    NULL,           0,         0,          1,		   0,		 -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	{ NULL,		"spterm",  NULL,		   SPTAG(0),  1,		  1,	       0,        -1 },
+	{ NULL,		"spfm",	   NULL,		   SPTAG(1),  1,		  0,	       1,        -1 },
+	{ NULL,		"keepassxc",NULL,		   SPTAG(2),  0,	      0,		   1,        -1 },
+ 	/* class      instance    title       tags mask     isfloating   monitor */
 };
 
 /* layout(s) */
